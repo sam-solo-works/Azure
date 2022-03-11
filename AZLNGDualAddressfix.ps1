39b196556e81e7b4e1097d9075ca845fafabc23b
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-11 16:23:56 -0500

    Rename AZLNGDualAddressfix.ps1 to Azure Local Network Gateway Dual Address issue fix.ps1

--- AZLNGDualAddressfix.ps1
+++
@@ -1,1 +0,0 @@
-﻿Set-AzLocalNetworkGateway -LocalNetworkGateway $lng -AddressPrefix @('10.10.10.10/32, 10.10.20.20/32') 
---
+++ Azure Local Network Gateway Dual Address issue fix.ps1
@@ -0,0 +1,1 @@
+﻿Set-AzLocalNetworkGateway -LocalNetworkGateway $lng -AddressPrefix @('10.10.10.10/32, 10.10.20.20/32') 
