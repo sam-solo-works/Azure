efb737fcea337600c5dacc87a0be6c580626b087
sam-solo-works <57299464+sam-solo-works@users.noreply.github.com>
2022-03-11 16:24:16 -0500

    Add files via upload

---
+++ AzureVnetCreationandPeeringtoCore.ps1
@@ -0,0 +1,62 @@
+﻿<#  Task: Automate creating azure Virtual networks and peers to core azure virtual network.
+    Want: Check for vnet prefix prior to implementing.
+          Add multiple tags at once.
+    Version: 1.5
+    Author: Sam Solmonson
+    DateWritten: 10/6/2021 DatePublished: -/-/- DateLastChangeMade: 10/12/2021
+#>  
+$azSub = 'test' #name of azure subscription
+$azSubCore = 'test' #name of azure core subscription
+$loc = 'test' #location of the resource group in azure
+$RG = 'test' # 'napa-eu2-nonprod01-davetest-rg' #name of Resource Group
+$coreRG = 'test' # name of core Resource Group
+$sNet = 'test' #name of subnet
+$vNet = 'test' #name of new virtual network
+$tags = @{
+'Created By'='Script'
+'Owner'='Creator' #Hash table created for tags
+'Project'='test'
+}
+$vNetPrefix = '192.168.255.0' #IPs of new virtual network 
+$sNetPrefix = '192.168.255.0/27' #IPs of new subnet of new virtual network
+$coreVNet = 'test'
+$coreVNetID = 'test'
+$peerCore = $corevnet + "_to_" + $vnet #naming convention for core side peer
+$peerNewVNet = $vNet + "_to_" + $coreVNet #naming convention for vnet side peer
+$vnetConnectName = $vnet + "_connection" #naming convention for vnet GW connection
+
+
+Connect-AzAccount
+Select-AzSubscription -SubscriptionName $azSub
+if (!(Get-AzVirtualNetwork -Name $vNet -ResourceGroupName $RG | Get-AzVirtualNetworkSubnetConfig | Where {$_.Name -like $sNet})){
+    $sNetConfig = New-AzVirtualNetworkSubnetConfig -Name $sNet -AddressPrefix $sNetPrefix 
+    New-AzVirtualNetwork -Name $vNet -ResourceGroupName $RG -Location $loc -AddressPrefix $vNetPrefix -Subnet $sNetConfig 
+    <#$addTags = (Get-AzVirtualNetwork -Name $vNet -ResourceGroupName $RG).Tag
+    foreach($key in $($addTags.keys)){
+        if(!$key){
+            if ($tags.ContainsKey($key)){
+            $addTags.Remove($key)
+             }
+            }
+        }
+    $addTags += $tags#>
+    $vNetObj = Get-AzVirtualNetwork -Name $vNet -ResourceGroupName $RG
+    #Add-AzVirtualNetworkPeering -Name $peerNewVNet -VirtualNetwork $vNetObj -RemoteVirtualNetworkId "/subscriptions/5228a252-f3a8-44a9-a1a0-066bad4ab7b4/resourceGroups/napa-eu2-core01-networking-rg/providers/Microsoft.Network/virtualNetworks/napa-eu2-core01-01-vnet"
+}    
+
+else{
+Write-Host "There is already a network with that config in place."
+}
+
+if (Get-AzVirtualNetwork -Name $vNet -ResourceGroupName $RG | Get-AzVirtualNetworkSubnetConfig | Where {$_.name -like $sNet}){
+Select-AzSubscription -SubscriptionName $azSubCore
+$localGW = Get-AzLocalNetworkGateway -Name test -ResourceGroupName $coreRG
+$vNetGW = Get-AzVirtualNetworkGateway -Name test -ResourceGroupName $coreRG
+$ipsec = New-AzIpsecPolicy -IkeEncryption AES256 -IkeIntegrity SHA256 -DhGroup DHGroup14 -IpsecEncryption AES256 -IpsecIntegrity SHA256 -PfsGroup None -SALifeTimeSeconds 14400 -SADataSizeKilobytes 102400000
+#Add-AzVirtualNetworkPeering -Name $peerCore -VirtualNetwork $coreVNet -RemoteVirtualNetworkId "test" -AllowGatewayTransit
+New-AzVirtualNetworkGatewayConnection -Name $vnetConnectName -VirtualNetworkGateway1 $vNetGW  -LocalNetworkGateway2 $localGW -ConnectionType IPsec -IpsecPolicies $ipsec
+}
+
+else{
+Write-Host "This network has not been built yet."
+}
